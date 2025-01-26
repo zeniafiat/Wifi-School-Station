cdc33b30a11a6aaae5d885a297b14e43d9fe65cd
zeniafiat <zeniafiat@gmail.com>
2025-01-26 14:23:17 +0300

    0.1.1

--- code_for_plata/code_for_plata.ino
+++ code_for_plata/code_for_plata.ino
@@ -1,32 +1,40 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include "DHT.h"
+
 #include <MQUnifiedsensor.h>
 
-#define DHTPIN 4
-#define DHTTYPE DHT11
-//*****
+#include <DHT11.h>
+
+
+//************WIFI**************
+const char* ssid = "Yura1";
+const char* password = "271183271183";
+const char* serverAddress = "192.168.1.212";
+const int serverPort = 8080;
+WiFiClient client;
+//***********************
+
+//*********MQ5**************
 #define         Board                   "ESP8266"
-#define         Pin                     A0  //Analog input 3 of your arduino
+#define         Pin                    A0  //Analog input 3 of your arduino
 /***********************Software Related Macros************************************/
 #define         Type                    "MQ-5" //MQ3
 #define         Voltage_Resolution      3.3 // 3V3 <- IMPORTANT
 #define         ADC_Bit_Resolution      10 // For ESP8266
 #define         RatioMQ5CleanAir        60
-
-const char* ssid = "Yura1";
-const char* password = "271183271183";
-const char* serverAddress = "192.168.1.212";
-const int serverPort = 8080;
-
+/*****************************Globals***********************************************/
 MQUnifiedsensor MQ5(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin, Type);
-WiFiClient client;
-DHT dht(DHTPIN, DHTTYPE);
+//***********************************
+
+//************DHT11****************
+DHT11 dht11(4);
+//*********************************
 
 void setup() {
+  //********************
   Serial.begin(115200);
+  //**********WIFI**********
   WiFi.begin(ssid, password);
-  dht.begin();
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
@@ -36,29 +44,41 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-//*********************************************************************
-   MQ5.setRegressionMethod(1); //_PPM =  a*ratio^b
-  MQ5.setA(521853); MQ5.setB(-3.821); // Configure the equation to to calculate Benzene concentration
-  MQ5.init(); 
-  Serial.print("Calibrating please wait.");
-  float calcR0 = 0;
-  for(int i = 1; i<=10; i ++)
-  {
-    MQ5.update(); // Update data, the arduino will read the voltage from the analog pin
-    calcR0 += MQ5.calibrate(RatioMQ5CleanAir);
-    Serial.print(".");
-  }
-  MQ5.setR0(calcR0/10);
-  Serial.println("  done!.");
-  
-  if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
-  if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
-  /*****************************  MQ CAlibration ********************************************/ 
-  MQ5.serialDebug(true);
-}
+//***************************MQ5******************************************
+      MQ5.setRegressionMethod(1); 
+      MQ5.setA(491204); MQ5.setB(-5.826);
+      /*
+        Exponential regression:
+      Gas    | a      | b
+      LPG    | 44771  | -3.245
+      CH4    | 2*10^31| 19.01
+      CO     | 521853 | -3.821
+      Alcohol| 0.3934 | -1.504
+      Benzene| 4.8387 | -2.68
+      Hexane | 7585.3 | -2.849
+      */
+      MQ5.init(); 
+      Serial.print("Calibrating please wait.");
+      float calcR0 = 0;
+      for(int i = 1; i<=10; i ++)
+      {
+        MQ5.update(); // Update data, the arduino will read the voltage from the analog pin
+        calcR0 += MQ5.calibrate(RatioMQ5CleanAir);
+        Serial.print(".");
+      }
+      MQ5.setR0(calcR0/10);
+      Serial.println("  done!.");
+      
+      if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
+      if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
+      /*****************************  MQ CAlibration ********************************************/ 
+      MQ5.serialDebug(true);
+  //*****************************************************************************************
 
+
+}
 void loop() {
-  //*************************************
+  //**************WIFI***********************
   if (!client.connected()) {
     if (client.connect(serverAddress, serverPort)) {
         Serial.println("Подключено к серверу");
@@ -67,19 +87,35 @@ void loop() {
       return; // Прекращаем этот цикл и ждём следующей итерации
     }
   }
-    //***************************************************
-    MQ5.update();
-    float t = dht.readTemperature();
-    float h = dht.readHumidity();
+
+  //*********************MQ5**********************
+    MQ5.update(); // Update data, the arduino will read the voltage from the analog pin
     float gas = MQ5.readSensor();
-    //*******
-    String message = "!temp:" + String(t) + "!hum:" + String(h) + "!CO:" + String(gas); // Добавляем время
-    //***********************************
+  //**********************************************
+
+  //********************DHT**************************
+      int temperature = 0;
+      int humidity = 0;
+      int result = dht11.readTemperatureHumidity(temperature, humidity);
+      if (result == 0) {
+          Serial.print("Temperature: ");
+          Serial.print(temperature);
+          Serial.print(" °C\tHumidity: ");
+          Serial.print(humidity);
+          Serial.println(" %");
+      } else {
+          Serial.println(DHT11::getErrorString(result));
+      }
+  //*************************************************
+
+    //*****************CONST**********************************
+    String message = "!temp:" + String(temperature) + "!hum:" + String(humidity) + "!CO:" + String(gas); // Добавляем время
+    //************MESEGE SENDER***********************
     if (client.print(message)) {
         Serial.println("Сообщение отправлено: " + message);
     } else {
         Serial.println("Не удалось отправить сообщение.");
     }
-    delay(500); // Повторяем попытку через 5 секунд (только если нужно)
+    delay(0.5); // Повторяем попытку через 5 секунд (только если нужно)
   
 }
--- server_code/bot/test.py
+++ server_code/bot/test.py
@@ -1,7 +1,24 @@
-data = "!temp:nan!hum:nan!CO2:0.10"
-d = data.split("!")
+import socket
 
-resultat = ""
-for i in d:
-    resultat += i + " "
-print(f"отправлено в хендлер: {resultat}")
+
+
+ip = "192.168.1.212"
+HOST = ip
+PORT = 8080
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Сервер запущен и слушает на {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print('Подключено от', addr)
+        data = None
+        while True:
+            data = conn.recv(4096)
+            data = data.decode()
+            print('Получено:', data)
+            d = data.split("!")
+            resultat = ""
+            for i in d:
+                resultat += i + " "
+    
