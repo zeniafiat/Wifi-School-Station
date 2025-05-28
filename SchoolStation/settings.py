bbef82f6c2978ea49089e1d9185b863cd51a66b8
zeniafiat <zeniafiat@gmail.com>
2025-05-29 01:14:44 +0300

    всё

--- SchoolStation/settings.py
+++ SchoolStation/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -122,8 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Папка с вашими статическими файлами
+STATIC_ROOT = BASE_DIR / "staticfiles"    # Папка для сборки статики (collectstatic)
 
+# Используйте whitenoise для обслуживания статики
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
