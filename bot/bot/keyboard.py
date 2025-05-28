3a893d940c6de0cb7a11f841f1d266e4ee59487a
zeniafiat <zeniafiat@gmail.com>
2025-05-28 19:43:47 +0300

    всё

--- bot/bot/keyboard.py
+++
@@ -1,5 +0,0 @@
-
-from aiogram.utils.keyboard import ReplyKeyboardMarkup, KeyboardButton
-
-
-keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Пришли данные')],[KeyboardButton(text='посмотреть прошлые данные')],[KeyboardButton(text='/set_id (здесь укажите айди)')]], resize_keyboard=True)
