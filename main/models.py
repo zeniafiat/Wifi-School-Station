512ddffe4907739240a86f5bfda144a7bdd1b3a3
zeniafiat <zeniafiat@gmail.com>
2025-05-31 13:41:22 +0300

    всё

--- main/models.py
+++ main/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import transaction
+from django.db.models import Max
 
 # Create your models here.
 class DATT(models.Model):
@@ -9,3 +13,16 @@ class DATT(models.Model):
     HUM = models.CharField(max_length=8)
     TEMP = models.CharField(max_length=8)
     time = models.TimeField(auto_now=True)
+
+@receiver(post_save, sender=DATT)
+def clean_old_records(sender, instance, created, **kwargs):
+    if created:
+        # Получаем 11-ю запись по времени (если существует)
+        to_delete = DATT.objects.filter(
+            addr=instance.addr,
+            room=instance.room
+        ).order_by('-time').values_list('id', flat=True)[10:]
+        
+        if to_delete.exists():
+            with transaction.atomic():
+                DATT.objects.filter(id__in=list(to_delete)).delete()
