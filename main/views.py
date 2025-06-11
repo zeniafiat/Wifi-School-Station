a078a50743c1c0cf373ab6dafcd3ddaef269232f
zeniafiat <zeniafiat@gmail.com>
2025-06-11 09:12:59 +0300

    пути

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ def index(request):
     context = {
         'title': 'School'
     }
-    return render(request, 'main\index.html',context=context)
+    return render(request, 'main/index.html',context=context)
 
 def docs(request):
     return render(request, 'main/docks.html')
